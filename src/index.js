1f04bf23af3208e56d19cb94766d7b075818e52f
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-04-25 16:37:41 +0300

    add start fail

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const refs = {
+    formSearch: document.querySelector('#search-form'),
+    input: document.querySelector('[type="text"]'),
+    btnSubmit: document.querySelector("[type='submit']"),
+    btnLoadMore: document.querySelector('.load-more'),
+    choiseScroll: document.querySelector(".scroll"),
+    choiseBtn: document.querySelector(".btn"),
+    gallery: document.querySelector('.gallery'),
+  };
