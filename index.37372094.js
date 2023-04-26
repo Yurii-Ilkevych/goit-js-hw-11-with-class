02586379c4c626d8667c3fe13d7e8de41d132df2
Yurii-Ilkevych <119433473+Yurii-Ilkevych@users.noreply.github.com>
2023-04-26 17:07:40 +0000

    Deploying to gh-pages from @ Yurii-Ilkevych/goit-js-hw-11-with-class@198e3cf9700d9756d30d52383c8f77fff568c8b5 🚀

--- index.37372094.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form"),document.querySelector('[type="text"]'),document.querySelector("[type='submit']"),document.querySelector(".load-more"),document.querySelector(".scroll"),document.querySelector(".btn"),document.querySelector(".gallery");
-//# sourceMappingURL=index.37372094.js.map
